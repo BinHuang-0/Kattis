a43f6f06a30892288a3888dffe1952115f913328
Bin Huang <binhuang295@gmail.com>
2019-08-31 21:25:21 -0500

    Added New Completed Challenge

---
+++ MicHiss.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Kattis
+{
+	class MicHiss
+	{
+		static void Main()
+		{
+			string input = Console.ReadLine();
+			int hiss = 0;
+
+			for(int i = 0; i < input.Length - 1; i++)
+			{
+				if (input.Substring(i, 1) == input.Substring(i + 1, 1) && input.Substring(i,1) == "s")
+				{
+					hiss = 1;
+				}
+			}
+
+			if (hiss == 1)
+				Console.WriteLine("hiss");
+			else
+				Console.WriteLine("no hiss");
+		}
+	}
+}
