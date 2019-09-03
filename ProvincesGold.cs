72eaefa62a1f421024a311a8588825ad1229c4e9
Bin Huang <binhuang295@gmail.com>
2019-09-03 10:59:06 -0500

    Added New Completed Challenge

---
+++ ProvincesGold.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Kattis
+{
+	class Provinces
+	{
+		static void ProvinceBuy()
+		{
+			string[] hand = Console.ReadLine().Split(' ');
+			int handVal = 3 * Int32.Parse(hand[0]) + 2 * Int32.Parse(hand[1]) + Int32.Parse(hand[2]);
+
+			if (handVal >= 8)
+				Console.Write("Province");
+			else if (handVal >= 5)
+				Console.Write("Duchy");
+			else if(handVal >= 2)
+				Console.Write("Estate");
+
+			if(handVal >= 2)
+				Console.Write(" or ");
+
+			if (handVal >= 6)
+				Console.WriteLine("Gold");
+			else if (handVal >= 3)
+				Console.WriteLine("Silver");
+			else
+				Console.WriteLine("Copper");
+		}
+	}
+}
