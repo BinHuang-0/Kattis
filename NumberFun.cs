89e9199b0f6b47ccdeb26430aa2e1614f903349c
Bin Huang <binhuang295@gmail.com>
2019-09-04 18:36:19 -0500

    Added New Completed Challenge

---
+++ NumberFun.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Kattis
+{
+	class NumberFun
+	{
+		static void Main()
+		{
+			int N = Int32.Parse(Console.ReadLine());
+			string[] input;
+
+			for(int i = 0; i < N; i++)
+			{
+				input = Console.ReadLine().Split(' ');
+				if (
+					Int32.Parse(input[0]) + Int32.Parse(input[1]) == Int32.Parse(input[2]) ||
+					Int32.Parse(input[0]) - Int32.Parse(input[1]) == Int32.Parse(input[2]) ||
+					Int32.Parse(input[0]) * Int32.Parse(input[1]) == Int32.Parse(input[2]) ||
+					(float)Int32.Parse(input[0]) / (float)Int32.Parse(input[1]) == (float)Int32.Parse(input[2]) ||
+					Int32.Parse(input[1]) + Int32.Parse(input[0]) == Int32.Parse(input[2]) ||
+					Int32.Parse(input[1]) - Int32.Parse(input[0]) == Int32.Parse(input[2]) ||
+					Int32.Parse(input[1]) * Int32.Parse(input[0]) == Int32.Parse(input[2]) ||
+					(float)Int32.Parse(input[1]) / (float)Int32.Parse(input[0]) == (float)Int32.Parse(input[2]) 
+					)
+					Console.WriteLine("Possible");
+
+				else
+					Console.WriteLine("Impossible");
+				
+			}
+		}
+	}
+}
