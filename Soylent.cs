a59a5c5228d13cf03f3f93169bf226b0d8f2de5b
Bin Huang <binhuang295@gmail.com>
2019-09-06 16:25:54 -0500

    Added New Completed Challenge

---
+++ Soylent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Kattis {
+	class Soy {
+		static void Main() {
+			int T = Int32.Parse(Console.ReadLine());
+			int cal;
+
+			for(int i = 0; i < T; i++) {
+				cal = Int32.Parse(Console.ReadLine());
+				Console.WriteLine(Math.Ceiling((double)cal / 400));
+			}
+		}
+	}
+}
